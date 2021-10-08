2b7f3f9614db8a14ed85d323ce6bcfc55f562fc9
kiran364 <kumawatkiran364@gmail.com>
2021-10-08 14:10:19 +0530

    javascript code to print positive and negative number

---
+++ positive.js
@@ -0,0 +1,23 @@
+let arr = [-9, 4, -10, 15, 19, -8, -2, 3, 3, 9, -15, 10, -20];
+let len = arr.length;
+let positive;
+
+for(let i = 0; i<len; i++){
+    if(arr[i] > 0){
+        for(let j= 0; j<arr.length; j++){
+            if(Math.sign(arr[j]) == -1){
+                if(Math.sign(arr[i]) == 1 &&  Math.sign(arr[j]) == -1){
+                    if(arr[i] == Math.abs(arr[j])){
+
+                        console.log(arr[i], arr[j])
+                    }
+                    
+                }
+                
+            }
+
+        }
+        
+    }
+
+}
